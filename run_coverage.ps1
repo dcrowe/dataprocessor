8a6170973e72064a8708e6679945d35dc9de4ca4
Daniel Crowe <crowe.daniel@gmail.com>
2018-04-17 22:20:18 +0930

    first attempt at coverage

---
+++ run_coverage.ps1
@@ -0,0 +1,10 @@
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
+    -register:user `
+    -target:"packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" `
+    -targetargs:"dataprocessor.tests\bin\Debug\dataprocessor.tests.dll" `
+    -output:".\coverage.xml" `
+    -filter:"+[dataprocessor*]* -[dataprocessor.tests*]*"
+
+.\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe `
+	-reports:coverage.xml `
+	-targetdir:coverage
