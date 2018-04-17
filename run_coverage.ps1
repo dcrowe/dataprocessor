09c6b2e60dcb11d3d3bae5e7c0d905767a839736
Daniel Crowe <crowe.daniel@gmail.com>
2018-04-18 06:55:04 +0930

    produce pdbs for release builds

--- run_coverage.ps1
+++ run_coverage.ps1
@@ -1,9 +1,9 @@
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
     -register:user `
     -target:"packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" `
-    -targetargs:"dataprocessor.tests\bin\Release\dataprocessor.tests.dll --result=TestResult.xml;format=AppVeyor" `
+    -targetargs:"-domain=single dataprocessor.tests\bin\Release\dataprocessor.tests.dll" `
     -output:".\coverage.xml" `
-    -filter:"+[dataprocessor*]* -[dataprocessor.tests*]*"
+    -filter:"+[dataprocessor]*"
 
 .\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe `
 	-reports:coverage.xml `
