a49af750da519b4c64339e9bb3e2c504340cdfc8
Daniel Crowe <crowe.daniel@gmail.com>
2018-04-17 22:49:44 +0930

    why failing on appveyor?

--- run_coverage.ps1
+++ run_coverage.ps1
@@ -1,7 +1,7 @@
 .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
     -register:user `
     -target:"packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" `
-    -targetargs:"dataprocessor.tests\bin\Debug\dataprocessor.tests.dll" `
+    -targetargs:"dataprocessor.tests\bin\Release\dataprocessor.tests.dll" `
     -output:".\coverage.xml" `
     -filter:"+[dataprocessor*]* -[dataprocessor.tests*]*"
 
