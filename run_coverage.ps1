4d93b27cde5eb2c1fa22df7dd89c641d6d3c525b
Daniel Crowe <crowe.daniel@gmail.com>
2018-08-30 19:48:35 +0930

    Fix up code coverage

--- run_coverage.ps1
+++ run_coverage.ps1
@@ -1,12 +1,20 @@
+
+
+# build the project
 dotnet build -c Release
 
-.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe `
+# obtain the nuget pkgs we need to run the coverage testing
+dotnet restore --packages .\packages .\dataprocessor.tests\
+
+# run the unit tests using the OpenCover
+.\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe `
     -register:user `
-    -target:"packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" `
+    -target:"packages\NUnit.ConsoleRunner\3.8.0\tools\nunit3-console.exe" `
     -targetargs:"-domain=single dataprocessor.tests\bin\Release\net461\dataprocessor.tests.dll" `
     -output:".\coverage.xml" `
     -filter:"+[dataprocessor]*"
 
-.\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe `
+# produce the report
+.\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe `
 	-reports:coverage.xml `
 	-targetdir:coverage
